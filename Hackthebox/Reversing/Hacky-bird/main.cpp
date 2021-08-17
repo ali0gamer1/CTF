52890b49a049436e22e1d4317646d3d0b8fdcc37
ali0gamer1 <69399548+ali0gamer1@users.noreply.github.com>
2021-08-17 09:49:22 +0430

    Update main.cpp

--- Hackthebox/Reversing/Hacky-bird/main.cpp
+++ Hackthebox/Reversing/Hacky-bird/main.cpp
@@ -129,23 +129,44 @@ int main()
     WriteProcessMemory(hprocess, (BYTE*)ammoaddr, &newammo, sizeof(newammo), 0);*/
 
     bool velocity_hack = false;
+    bool collision_hack = false;
     while (1)
     {
 
-        if (GetAsyncKeyState(VK_UP) & 1)
+        if (GetAsyncKeyState(0x56) & 1) //V key (velocity)
         {
             velocity_hack = !velocity_hack;
 
             if (velocity_hack)
             {
+                std::vector<unsigned int> velocity;
+                velocity.push_back(0x2c);
+                uintptr_t velocity_addr = findDMAaddr(hprocess, dynamicptr, velocity);
+                static int new_vel  = 0;
+                WriteProcessMemory(hprocess, (BYTE*)velocity_addr, &new_vel, sizeof(new_vel), 0);
                 patch((BYTE*)(modulebase + 0x2fe0), (BYTE*)"\x90\x90\x90", 3, hprocess);
             }
             else
             {
+                static int new_vel = 5;
+                WriteProcessMemory(hprocess, (BYTE*)velocity_addr, &new_vel, sizeof(new_vel), 0);
                 patch((BYTE*)(modulebase + 0x2fe0), (BYTE*)"\xff\x46\x2c", 3, hprocess);
             }
+        }
 
-            
+
+        if (GetAsyncKeyState(0x43) & 1) //C key (collision)
+        {
+            collision_hack = !collision_hack;
+
+            if (collision_hack)
+            {
+                patch((BYTE*)(modulebase + 0x30BF), (BYTE*)"\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x31\xc0\x90\x90\x90\x90", 61, hprocess);
+            }
+            else
+            {
+                patch((BYTE*)(modulebase + 0x30BF), (BYTE*)"\x8d\x45\xcc\x50\x8d\x45\xdc\x89\x55\xcc\x50\x8d\x45\xac\xc7\x45\xd0\x00\x00\x00\x00\x50\x89\x55\xbc\x0f\x11\x45\xac\xff\x15\x80\x21\x41\x00\x85\xc0\x0f\x85\xcd\x00\x00\x00\x8d\x45\xbc\x50\x8d\x45\xdc\x50\x8d\x45\xac\x50\xff\x15\x80\x21\x41\x00", 61, hprocess);
+            }
         }
 
     }
